d3f4b80e62649aa21b33ddd969606473fe034ebb
Mike <mcataldo@3plcentral.com>
2020-09-28 08:11:07 -0600

    add login spec

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('login', () => {
+  it('login with username and password', () => {
+    cy.visit('localhost:3000/signin')
+    cy.get('#username').type('Allie2')
+    cy.get('#password').type('s3cret')
+    cy.get('button:contains("Sign In")').click()
+  })
+})

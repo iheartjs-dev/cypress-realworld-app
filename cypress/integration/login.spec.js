0fad44785f2098b2b6e73b6ba1c8e3047f1c574a
Mike <mcataldo@3plcentral.com>
2020-09-29 11:58:03 -0600

    apply style rules to login spec

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,14 @@
 describe('sign in', () => {
   it('sign in with username and password', () => {
     cy.visit('/signin')
-    cy.get('#username').type('Allie2')
-    cy.get('button:contains("Sign In")').should('be.disabled').and('have.class', 'Mui-disabled')
-    cy.get('#password').type('s3cret')
-    cy.get('button:contains("Sign In")').click()
+    cy.get('#username')
+      .type('Allie2')
+    cy.get('button:contains("Sign In")')
+      .should('be.disabled')
+      .and('have.class', 'Mui-disabled')
+    cy.get('#password')
+      .type('s3cret')
+    cy.get('button:contains("Sign In")')
+      .click()
   })
 })

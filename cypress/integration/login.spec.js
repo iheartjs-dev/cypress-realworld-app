dec1c9fde434fb7659316c14d86ab437a8d9b051
Mike <mcataldo@3plcentral.com>
2020-09-29 11:06:24 -0600

    update login.spec.js

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,8 @@
-describe('login', () => {
-  it('login with username and password', () => {
-    cy.visit('localhost:3000/signin')
+describe('sign in', () => {
+  it('sign in with username and password', () => {
+    cy.visit('/signin')
     cy.get('#username').type('Allie2')
+    cy.get('button:contains("Sign In")').should('be.disabled').and('have.class', 'Mui-disabled')
     cy.get('#password').type('s3cret')
     cy.get('button:contains("Sign In")').click()
   })

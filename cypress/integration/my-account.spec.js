ef31be3a061431eaa35706e1bc5b6aa48d61081d
Mike <mcataldo@3plcentral.com>
2020-11-03 16:34:09 -0700

    add hook to nav and test to update account

--- cypress/integration/my-account.spec.js
+++ cypress/integration/my-account.spec.js
@@ -6,29 +6,39 @@ describe('My Account', () => {
   beforeEach('sign in', () => {
     signIn(data.username, data.password)
   })
-
-  function signIn(username, password) {
-    cy.request({
-      method: 'POST',
-      url: '/login',
-      body: {
-        type: 'LOGIN',
-        username: username,
-        password: password,
-        remember: true
-      },
-    })
-      .its('body.user')
-      .then(user => {
-        const authState = {
-          value: 'authorized',
-          context: { user },
-          _event: {},
-          event: {
-            data: { user }
-          }
-        }
-        window.localStorage.setItem('authState', JSON.stringify(authState))
-      })
-  }
+  beforeEach('nav to My Account', () => {
+    cy.visit('/user/settings')
+    cy.wait([
+      '@getNotifications',
+      '@getBankAccounts'
+    ])
+  })
+  it('update account information', () => {
+    cy.get('[data-test=user-settings-email-input]')
+  })
 })
+
+function signIn(username, password) {
+  cy.request({
+    method: 'POST',
+    url: '/login',
+    body: {
+      type: 'LOGIN',
+      username: username,
+      password: password,
+      remember: true
+    },
+  })
+    .its('body.user')
+    .then(user => {
+      const authState = {
+        value: 'authorized',
+        context: { user },
+        _event: {},
+        event: {
+          data: { user }
+        }
+      }
+      window.localStorage.setItem('authState', JSON.stringify(authState))
+    })
+}

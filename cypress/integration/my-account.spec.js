b2bcc1881363d3cff1b4116a8270257a6770daa9
Mike <mcataldo@3plcentral.com>
2020-11-03 16:28:15 -0700

    add `describe`, `beforeEach` that invokes `signIn`

--- cypress/integration/my-account.spec.js
+++ cypress/integration/my-account.spec.js
@@ -1,24 +1,34 @@
-function signIn(username, password) {
-  cy.request({
-    method: 'POST',
-    url: '/login',
-    body: {
-      type: 'LOGIN',
-      username: username,
-      password: password,
-      remember: true
-    },
+describe('My Account', () => {
+  const data = {
+    username: 'Allie2',
+    password: 's3cret'
+  }
+  beforeEach('sign in', () => {
+    signIn(data.username, data.password)
   })
-    .its('body.user')
-    .then(user => {
-      const authState = {
-        value: 'authorized',
-        context: { user },
-        _event: {},
-        event: {
-          data: { user }
-        }
-      }
-      window.localStorage.setItem('authState', JSON.stringify(authState))
+
+  function signIn(username, password) {
+    cy.request({
+      method: 'POST',
+      url: '/login',
+      body: {
+        type: 'LOGIN',
+        username: username,
+        password: password,
+        remember: true
+      },
     })
-}
+      .its('body.user')
+      .then(user => {
+        const authState = {
+          value: 'authorized',
+          context: { user },
+          _event: {},
+          event: {
+            data: { user }
+          }
+        }
+        window.localStorage.setItem('authState', JSON.stringify(authState))
+      })
+  }
+})

36efcf1c9acea8f47a6b04954ce17d8a9feb6702
Mike <mcataldo@3plcentral.com>
2020-11-03 14:02:25 -0700

    add tests on home

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,8 @@
 describe('home', () => {
   const data = {
     username: 'Allie2',
-    password: 's3cret'
+    password: 's3cret',
+    fullName: 'Kaylin H'
   }
   beforeEach('setup routes', () => {
     cy.server()
@@ -41,6 +42,32 @@ describe('home', () => {
       })
     cy.visit('/')
   })
+
+  describe('side nav', () => {
+    it('name, handle and avatar are visible', () => {
+      cy.get('[data-test=sidenav-user-full-name]')
+        .should('be.visible')
+        .and('have.text', data.fullName)
+      cy.get('[data-test=sidenav-username]')
+        .should('be.visible')
+        .and('contain.text', data.username)
+    })
+    it('balance amount and its subtitle are visible', () => {
+      cy.get('[data-test=sidenav-user-balance]')
+        .should('be.visible')
+      cy.contains('h6', 'Account Balance')
+        .should('be.visible')
+    })
+    it('side nav links are visible and correct', () => {
+      cy.contains('a', 'Home')
+        .should('have.attr', 'href', '/')
+      cy.contains('a', 'My Account')
+        .should('have.attr', 'href', '/user/settings')
+      cy.contains('a', 'Bank Accounts')
+        .should('have.attr', 'href', '/bankaccounts')
+      cy.contains('a', 'Notifications')
+        .should('have.attr', 'href', '/notifications')
+    })
   it('log out', () => {
     cy.contains('Logout')
       .click()

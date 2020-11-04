1f85f0a28558d16898ccd878f95e73bd3f1fcc78
Mike <mcataldo@3plcentral.com>
2020-11-03 18:15:20 -0700

    create auth support module containing `signIn`

---
+++ cypress/support/auth.js
@@ -0,0 +1,24 @@
+export function signIn(username, password) {
+  cy.request({
+    method: 'POST',
+    url: '/login',
+    body: {
+      type: 'LOGIN',
+      username: username,
+      password: password,
+      remember: true
+    },
+  })
+    .its('body.user')
+    .then(user => {
+      const authState = {
+        value: 'authorized',
+        context: { user },
+        _event: {},
+        event: {
+          data: { user }
+        }
+      }
+      window.localStorage.setItem('authState', JSON.stringify(authState))
+    })
+}

88e270567f6ce058453004c620892c7d3a7f39e0
Mike <mcataldo@3plcentral.com>
2020-11-17 10:22:34 -0700

    update home spec, use global hooks, update `data`

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,45 +1,10 @@
 describe('home', () => {
   const data = {
-    username: 'Allie2',
-    password: 's3cret',
+    username: Cypress.env('username'),
     fullName: 'Kaylin H'
   }
-  beforeEach('setup routes', () => {
-    cy.server()
-    cy.route('POST', '/login')
-      .as('login')
-    cy.route('GET', '/bankAccounts')
-      .as('getBankAccounts')
-    cy.route('GET', '/transactions/public')
-      .as('getPublicTransactions')
-    cy.route('GET', '/notifications')
-      .as('getNotifications')
-    cy.route('POST', '/logout')
-      .as('logout')
-  })
-  beforeEach('sign in', () => {
-    cy.request({
-      method: 'POST',
-      url: '/login',
-      body: {
-        type: 'LOGIN',
-        username: data.username,
-        password: data.password,
-        remember: true
-      },
-    })
-      .its('body.user')
-      .then(user => {
-        const authState = {
-          value: 'authorized',
-          context: { user },
-          _event: {},
-          event: {
-            data: { user }
-          }
-        }
-        window.localStorage.setItem('authState', JSON.stringify(authState))
-      })
+
+  beforeEach('navigate to Home', () => {
     cy.visit('/')
   })
 
